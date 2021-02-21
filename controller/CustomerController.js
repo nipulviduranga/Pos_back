f2a2d78a2c70cfd5b2ddd8dd202e99e0c3b8e3a7
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-02-21 12:35:02 +0530

    5

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -33,7 +33,23 @@ const updateCustomer = (req, resp) => {
 }
 
 const deleteCustomer = (req, resp) => {
+    try{
 
+        CustomerDTO.deleteOne({_id:req.headers.id}).then(result=>{
+
+            if (result.deletedCount>0){
+                resp.status(200).json({message: 'Deleted!'});
+            }else{
+                resp.status(500).json({message: 'Try Again!'});
+            }
+
+        }).catch(error=>{
+            resp.status(500).json({error: error});
+        })
+
+    }catch (e) {
+        resp.status(500).json({error: e});
+    }
 }
 
 const getCustomer = (req, resp) => {
--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -3,10 +3,12 @@ const CustomerController =  require('../controller/CustomerController');
 
 const router = express.Router();
 
-//post (save)
+//post (save) [body]
 router.post('/saveCustomer', CustomerController.saveCustomer); /*saveCustomer()*/
-//get(fetch)
+//get(fetch) [headers]
 router.get('/getAllCustomers', CustomerController.getAllCustomers);
+//delete(delete) [headers]
+router.delete('/deleteCustomer', CustomerController.deleteCustomer);
 
 
 module.exports = router;
