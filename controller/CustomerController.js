f3f42515fb0516e14d8a75c9be28ba90b34d3333
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-02-21 12:16:37 +0530

    4

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -41,7 +41,20 @@ const getCustomer = (req, resp) => {
 }
 
 const getAllCustomers = (req, resp) => {
-    console.log('ok')
+    try {
+        // pagination
+        CustomerDTO.find().then(result=>{
+
+            resp.status(200).json({dataSet: result});
+
+        }).catch(exception=>{
+            resp.status(500).json({error: exception});
+        })
+
+    } catch (e) {/*JSON*/
+        /*resp.status(500).json(e);*/
+        resp.status(500).json({error: e});
+    }
 }
 
 module.exports = {
--- routes/CustomerRoute.js
+++ routes/CustomerRoute.js
@@ -3,7 +3,10 @@ const CustomerController =  require('../controller/CustomerController');
 
 const router = express.Router();
 
+//post (save)
 router.post('/saveCustomer', CustomerController.saveCustomer); /*saveCustomer()*/
+//get(fetch)
+router.get('/getAllCustomers', CustomerController.getAllCustomers);
 
 
 module.exports = router;
